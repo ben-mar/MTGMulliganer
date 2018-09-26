0d3edbf0f00084e0b385b5b207f0a4d7917a6735
gogole3000 <39266506+gogole3000@users.noreply.github.com>
2018-09-26 17:46:39 +0200

    add more bant spirit examples

--- magic.py
+++ magic.py
@@ -2,10 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from PIL import Image
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.externals import joblib
 from sklearn.model_selection import train_test_split
-
+from sklearn.ensemble import RandomForestClassifier
 
 class Deck:
 
